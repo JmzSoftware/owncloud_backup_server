d481aa2b27404864fe526cfd7611ec3ebbf15846
James <jmztaylor@gmail.com>
2016-10-26 13:44:48 -0500

    Remove verbose and add preserve permissions

--- backup.sh
+++ backup.sh
@@ -15,7 +15,7 @@ backup_to_oc_dir='' # This is the folder name on OwnCloud
 
 mysqldump -h$mysql_host -uroot --all-databases > $backupname.sql
 
-tar cvfz $script_path/$backupname.tar.gz $dir_to_backup
+tar cpfz $script_path/$backupname.tar.gz $dir_to_backup
 curl -X PUT -u $oc_username:$oc_password --data-binary @"$backupname.tar.gz" "$oc_server/remote.php/webdav/$backup_to_oc_dir/$backupname.tar.gz"
 curl -X PUT -u $oc_username:$oc_password --data-binary @"$backupname.sql" "$oc_server/remote.php/webdav/$backup_to_oc_dir/$backupname.sql"
 rm $script_path/$backupname.sql
